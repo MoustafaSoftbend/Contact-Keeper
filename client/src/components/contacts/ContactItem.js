87018d193da198717f4c7e25e74972009a3f95be
Moustafa <bendib.moustafa@gmail.com>
2019-10-28 18:18:42 +0100

    Finalizing CRUD functionality and adding input filtering functionality

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import PropTypes from 'prop-types'
+import React, {useContext} from 'react';
+import PropTypes from 'prop-types';
+import ContactContext from '../../context/contact/contactContext'
 
 
 const ContactItem = ({ contact }) =>{
+    const contactContext = useContext(ContactContext) 
+
     const {id , name, email, phone, type} = contact;
+
+    const {deleteContact, setCurrent, clearCurrent} = contactContext;
+
+    const onDelete = () => {
+        deleteContact(id);
+        clearCurrent()
+    }
 
     return (
         <div className="card bg-light">
@@ -19,8 +29,8 @@ const ContactItem = ({ contact }) =>{
                 </li>)}
             </ul>
             <p>
-                <button className="btn btn-dark btn-sm">Edit</button>
-                <button className="btn btn-danger btn-sm">Delete</button>
+                <button className="btn btn-dark btn-sm" onClick={() => setCurrent(contact)}>Edit</button>
+                <button className="btn btn-danger btn-sm" onClick={onDelete}>Delete</button>
             </p>
         </div>
     )
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Contacts from '../contacts/Contacts'
+import ContactForm from '../contacts/ContactForm';
+import ContactsFilter from '../contacts/ContactsFilter';
 
 const Home = () =>{
     return (
         <div className="grid-2">
             <div>
-                {/* ContactForm */}
+                < ContactForm />
             </div>
             <div>
+                <ContactsFilter />
                 <Contacts />
             </div>
         </div>
