55be7862e7e3faf164c76f31f9469ba02152e60c
Moustafa <bendib.moustafa@gmail.com>
2019-10-30 12:09:20 +0100

    connecting user auth rfont end with backend

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,6 +1,28 @@
-import React,{useState} from 'react';
+import React,{useState, useContext, useEffect} from 'react';
+import AuthContext from '../../context/auth/authContext';
+import AlertContext from '../../context/alert/alertContext';
 
-const Login = () =>{
+const Login = props =>{
+
+    const authContext = useContext(AuthContext)
+
+    const alertContext = useContext(AlertContext);
+
+    const {login, error, clearErrors, isAuthenticated} = authContext;
+
+    const {setAlert} = alertContext;
+
+    useEffect(()=>{
+        if(isAuthenticated){
+            props.history.push('/');
+        }
+        
+        if(error ==='Invalid Credentials'){
+            setAlert(error, 'danger');
+            clearErrors();
+        }
+        // eslint-disable-next-line
+    }, [error, isAuthenticated, props.history]);
 
     const [user, setUser] = useState({
         email: '',
@@ -12,8 +34,15 @@ const Login = () =>{
     const onChange = e => setUser({...user, [e.target.name]: e.target.value});
 
     const onSubmit = e => {
-        e.preventDeafult();
-        console.log('Login submit')
+        e.preventDefault();
+        if(email==='' || password ===''){
+            setAlert('Please fill in all fields', 'danger');
+        } else {
+            login({
+                email,
+                password
+            })
+        }
     }
 
     return (
