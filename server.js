3a84fa2fdeaea6bb74fc54ad26e08f239db1d0dd
Moustafa <bendib.moustafa@gmail.com>
2019-10-31 11:58:08 +0100

    Fixed server Bug not found

--- server.js
+++ server.js
@@ -19,10 +19,15 @@ app.use('/api/contacts', require('./routes/contacts'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production'){
-    // Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, './client/build')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+    app.get('*', function(_, res) {
+      res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+        if (err) {
+          res.status(500).send(err)
+        }
+      })
+    })
 }
 
 const PORT = process.env.PORT || 5000;
