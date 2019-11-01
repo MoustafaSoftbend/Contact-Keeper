42efb35fe3e61a826ebaeff6c32b4325d63a4ed4
Moustafa <bendib.moustafa@gmail.com>
2019-11-01 18:57:22 +0100

    Final Tray 2

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const connectDB = require('../config/db');
+const connectDB = require('./config/db');
 const path = require('path');
 
 const app = express();
@@ -11,11 +11,11 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // Define Routes
-app.use('/api/users', require('../routes/users'));
+app.use('/api/users', require('./routes/users'));
 
-app.use('/api/auth', require('../routes/auth'));
+app.use('/api/auth', require('./routes/auth'));
 
-app.use('/api/contacts', require('../routes/contacts'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production'){
@@ -25,6 +25,6 @@ if (process.env.NODE_ENV === 'production'){
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
